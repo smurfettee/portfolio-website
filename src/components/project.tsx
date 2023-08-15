f3cb2ef1f1c5d60fba88f3c95f65286523552152
Ege <egekaracetin35@hotmail.com>
2023-08-15 02:26:50 +0200

    Finished portfolio component.

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -9,17 +9,17 @@ interface props {
 
 export default function Project(props:props){
     return (
-        <div className="flex flex-col items-center m-14">
-            <div>{props.name}</div>
-            <div>{props.content}</div>
-            <div>
+        <div className="flex flex-col items-center m-8 p-4 rounded-lg shadow-[0_0_10px_rgba(0,0,0,0.1)] [&>div]:mb-5">
+            <div className="text-xl font-bold ">{props.name}</div>
+            <div className="text-gray-600 text-center w-[90%] ">{props.content}</div>
+            <div className="flex flex-wrap">
                 {
                     props.technologies.map(tech => (
-                        <div>{tech}</div>
+                        <div className="m-2 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-lg p-2 font-bold">{tech}</div>
                     ))
                 }
             </div>
-            <a href={props.github} target="_blank">Code {github}</a>
+            <a href={props.github} target="_blank" className="mb-5 text-lg hover:text-blue-600 duration-200">Code {github}</a>
         </div>
     );
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,12 +4,24 @@ export default function Projects(){
     return (
         <div className="flex flex-col items-center">
             <div className="text-blue-600 text-xl font-extrabold mb-8">Portfolio</div>
-            <div className="font-bold text-lg">These are a few of the projects that I've done</div>
+            <div className="font-bold text-lg mb-10">These are a few of the projects that I've done</div>
             <Project 
                 name="Coindom" 
                 content="Coinsafe is a website that allows users to search for information about various cryptocurrencies in real-time. The website is designed to provide users with accurate and up-to-date information about the price, market cap, trading volume, and other key metrics of cryptocurrencies."
                 technologies={["React", "Tailwind"]}
                 github="https://github.com/smurfettee/Crypto-Site"
+            />
+            <Project
+                name="Personal Blog"
+                content="A personal blog website where users can login, register, post and edit their blogs and see other peoples blogs on the main page."
+                technologies={["React", "MongoDB", "ExpressJS"]}
+                github="https://github.com/smurfettee/MERN-Blog-Frontend"
+            />
+            <Project
+                name="Typing Speed Game"
+                content="A typing speed game that the user tries to write the duplicate of the sentence that appear on the screen also getting the words per minute as a result."
+                technologies={["Javascript", "HTML", "CSS"]}
+                github="https://github.com/smurfettee/Typing-Speed-Game"
             />
         </div>
     );
